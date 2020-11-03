6e744c246290f4f5898e3a50ed123f0298626f34
Deddy Romnan <romnandeddy@gmail.com>
2020-11-03 16:24:35 +0700

    added sectionpager UI

--- app/src/main/java/com/romnan/moviecatalog/ui/catalog/CatalogActivity.kt
+++ app/src/main/java/com/romnan/moviecatalog/ui/catalog/CatalogActivity.kt
@@ -1,12 +1,25 @@
 package com.romnan.moviecatalog.ui.catalog
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.romnan.moviecatalog.R
+import kotlinx.android.synthetic.main.activity_catalog.*
 
 class CatalogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalog)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        view_pager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(view_pager)
+
+        supportActionBar?.elevation = 0f
+
+        // Change action bar color
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimaryDark)))
     }
 }
