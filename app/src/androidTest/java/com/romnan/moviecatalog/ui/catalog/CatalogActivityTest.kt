900b3ac80de50f338db86c65a765b63594035afd
Deddy Romnan <romnandeddy@gmail.com>
2020-11-23 22:10:44 +0700

    adjust RecyclerView to rv_pop_show

--- app/src/androidTest/java/com/romnan/moviecatalog/ui/catalog/CatalogActivityTest.kt
+++ app/src/androidTest/java/com/romnan/moviecatalog/ui/catalog/CatalogActivityTest.kt
@@ -1,18 +1,14 @@
 package com.romnan.moviecatalog.ui.catalog
 
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.InstrumentationRegistry
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.internal.inject.InstrumentationContext
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import com.romnan.moviecatalog.R
 import com.romnan.moviecatalog.util.DataDummy
-import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 
@@ -26,8 +22,8 @@ class CatalogActivityTest {
 
     @Test
     fun loadMovies() {
-        onView(withId(R.id.rv_movie)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_movie)).perform(
+        onView(withId(R.id.rv_pop_show)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_pop_show)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
                 dummyMovies.size
             )
@@ -37,8 +33,8 @@ class CatalogActivityTest {
     @Test
     fun loadTvShows() {
         onView(withText("TV Shows")).perform(click())
-        onView(withId(R.id.rv_tv_show)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_tv_show)).perform(
+        onView(withId(R.id.rv_pop_show)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_pop_show)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
                 dummyTvShows.size
             )
