b2046ea1655ffebd9214f30aa6de7a6529e2f8f7
Deddy Romnan <romnandeddy@gmail.com>
2020-11-24 16:39:17 +0700

    Renamed

--- app/src/main/java/com/romnan/moviecatalog/ui/tvshows/TvShowsViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.romnan.moviecatalog.ui.tvshows
-
-import androidx.lifecycle.ViewModel
-import com.romnan.moviecatalog.model.TvShow
-import com.romnan.moviecatalog.util.DataDummy
-
-class TvShowsViewModel : ViewModel() {
-    fun retrieveTvShowData(): ArrayList<TvShow> = DataDummy.generateDummyTvShows()
-}
