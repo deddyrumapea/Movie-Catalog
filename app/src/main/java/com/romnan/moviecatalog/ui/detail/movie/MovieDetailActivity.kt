7c0940c06d87c63e1c5ad6a878c52dcb796ccd40
Deddy Romnan <romnandeddy@gmail.com>
2020-11-27 21:55:06 +0700

    adjust movie detail data to layout

--- app/src/main/java/com/romnan/moviecatalog/ui/detail/movie/MovieDetailActivity.kt
+++ app/src/main/java/com/romnan/moviecatalog/ui/detail/movie/MovieDetailActivity.kt
@@ -5,20 +5,30 @@ import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.romnan.moviecatalog.R
-import com.romnan.moviecatalog.model.Movie
+import com.romnan.moviecatalog.model.MovieDetail
 import kotlinx.android.synthetic.main.activity_movie_detail.*
-import kotlinx.android.synthetic.main.dialog_error.*
+import kotlinx.android.synthetic.main.activity_movie_detail.button_trailer
+import kotlinx.android.synthetic.main.activity_movie_detail.progress_bar_score
+import kotlinx.android.synthetic.main.activity_movie_detail.text_duration
+import kotlinx.android.synthetic.main.activity_movie_detail.text_language
+import kotlinx.android.synthetic.main.activity_movie_detail.text_overview
+import kotlinx.android.synthetic.main.activity_movie_detail.text_score
+import kotlinx.android.synthetic.main.activity_movie_detail.text_status
+import kotlinx.android.synthetic.main.activity_movie_detail.text_tagline
+import kotlinx.android.synthetic.main.activity_tv_show_detail.*
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 class MovieDetailActivity : AppCompatActivity() {
 
     companion object {
-        const val EXTRA_MOVIE = "extra_movie"
+        const val EXTRA_MOVIE_ID = "extra_movie_id"
+        private const val TAG = "MovieDetailActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,20 +40,22 @@ class MovieDetailActivity : AppCompatActivity() {
         supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.colorPrimaryDark)))
         supportActionBar?.title = getString(R.string.movie_detail)
 
+        // Setup ViewModel
         val viewModel = ViewModelProvider(
             this,
             ViewModelProvider.NewInstanceFactory()
         )[MovieDetailViewModel::class.java]
 
+        // Get intent extras
         val extras = intent.extras
         if (extras != null) {
-            val movieId = extras.getString(EXTRA_MOVIE)
+            val movieId = extras.getString(EXTRA_MOVIE_ID)
             if (movieId != null) {
-                viewModel.setSelectedMovie(movieId)
-                populateMovieDetails(viewModel.getMovie())
+                viewModel.getMovieDetail(movieId)
             } else showErrorDialog()
         } else showErrorDialog()
 
+        viewModel.movieDetail.observe(this, { populateMovieDetails(it) })
     }
 
     private fun showErrorDialog() {
@@ -54,38 +66,43 @@ class MovieDetailActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    private fun populateMovieDetails(movie: Movie) {
+    private fun populateMovieDetails(movie: MovieDetail) {
         Glide.with(this)
-            .load(movie.poster)
+            .load(String.format(getString(R.string.image_base_url), movie.posterPath))
             .into(image_movie_poster)
 
         Glide.with(this)
-            .load(movie.poster)
-            .into(image_movie_background)
+            .load(String.format(getString(R.string.image_base_url), movie.backdropPath))
+            .into(image_movie_backdrop)
 
         text_movie_title.text = movie.title
-        text_score.text = movie.score.toString()
-        progress_bar_score.progress = movie.score
-        button_play_trailer.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(movie.trailerUrl))
+        text_score.text = movie.voteAverage.toString()
+        progress_bar_score.progress = (movie.voteAverage * 10).roundToInt()
+        button_trailer.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW).setData(
+                Uri.parse(
+                    String.format(
+                        getString(R.string.youtube_base_url),
+                        movie.videos.results[0].key
+                    )
+                )
+            )
             startActivity(intent)
         }
 
-        text_release_date.text = movie.releaseDate
-        text_duration.text = movie.duration
-        text_genre.text = movie.genre
-        if (movie.tagline != "") {
-            text_tagline.text = movie.tagline
-        } else {
-            text_tagline.visibility = View.GONE
+        var genre = ""
+        movie.genres.forEach {
+            genre += " ${it.name} "
         }
+        text_genre_movie.text = genre
+
+        text_release_date.text = movie.releaseDate
+        text_duration.text = String.format(getString(R.string.runtime_format), movie.runtime)
+        text_tagline.text = movie.tagline
         text_overview.text = movie.overview
-        text_director.text = movie.director
-        text_casts.text = movie.casts
         text_status.text = movie.status
-        text_language.text = movie.language
-        text_budget.text = movie.budget
-        text_revenue.text = movie.revenue
-        text_keywords.text = movie.keyword
+        text_language.text = movie.spokenLanguages[0].name
+        text_budget.text = DecimalFormat("#,###").format(movie.budget)
+        text_revenue.text = DecimalFormat("#,###").format(movie.revenue)
     }
 }
