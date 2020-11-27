955dc4d7a80df674b6ccd727853265ce67ed34d1
Deddy Romnan <romnandeddy@gmail.com>
2020-11-27 21:55:39 +0700

    adjust tv show detail data to layout

--- app/src/main/java/com/romnan/moviecatalog/ui/detail/tvshows/TvShowDetailActivity.kt
+++ app/src/main/java/com/romnan/moviecatalog/ui/detail/tvshows/TvShowDetailActivity.kt
@@ -5,20 +5,19 @@ import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
-import android.view.View.GONE
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.romnan.moviecatalog.R
-import com.romnan.moviecatalog.model.TvShow
+import com.romnan.moviecatalog.model.TvShowDetail
 import kotlinx.android.synthetic.main.activity_tv_show_detail.*
-import kotlinx.android.synthetic.main.dialog_error.*
+import kotlin.math.roundToInt
 
 class TvShowDetailActivity : AppCompatActivity() {
 
     companion object {
-        const val EXTRA_TV_SHOW = "extra_tv_show"
+        const val EXTRA_TV_SHOW_ID = "extra_tvShow_id"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,21 +29,22 @@ class TvShowDetailActivity : AppCompatActivity() {
         supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.colorPrimaryDark)))
         supportActionBar?.title = getString(R.string.tv_show_detail)
 
-        // Create viewholder
+        // Setup ViewModel
         val viewModel = ViewModelProvider(
             this,
             ViewModelProvider.NewInstanceFactory()
         )[TvShowDetailViewModel::class.java]
 
-        // Get extra data
+        // Get intent extras
         val extras = intent.extras
         if (extras != null) {
-            val tvShowId = extras.getString(EXTRA_TV_SHOW)
+            val tvShowId = extras.getString(EXTRA_TV_SHOW_ID)
             if (tvShowId != null) {
-                viewModel.setSelectedTvShow(tvShowId)
-                populateTvShowDetails(viewModel.getTvShow())
+                viewModel.getTvShowDetail(tvShowId)
             } else showErrorDialog()
         } else showErrorDialog()
+
+        viewModel.tvShowDetail.observe(this, { populateTvShowDetails(it) })
     }
 
     private fun showErrorDialog() {
@@ -55,36 +55,53 @@ class TvShowDetailActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    private fun populateTvShowDetails(tvShow: TvShow) {
+    private fun populateTvShowDetails(tvShow: TvShowDetail) {
         Glide.with(this)
-            .load(tvShow.poster)
-            .into(image_tvshow_background)
-
-        Glide.with(this)
-            .load(tvShow.poster)
+            .load(String.format(getString(R.string.image_base_url), tvShow.posterPath))
             .into(image_tvshow_poster)
 
-        text_tvshow_title.text = tvShow.title
-        text_score.text = tvShow.score.toString()
-        progress_bar_score.progress = tvShow.score
-        button_play_trailer.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(tvShow.trailerUrl))
+        Glide.with(this)
+            .load(String.format(getString(R.string.image_base_url), tvShow.backdropPath))
+            .into(image_tvshow_backdrop)
+
+        text_tvshow_title.text = tvShow.name
+        text_score.text = tvShow.voteAverage.toString()
+        progress_bar_score.progress = (tvShow.voteAverage * 10).roundToInt()
+        button_trailer.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW).setData(
+                Uri.parse(
+                    String.format(
+                        getString(R.string.youtube_base_url),
+                        tvShow.videos.results[0].key
+                    )
+                )
+            )
             startActivity(intent)
         }
-        text_duration.text = tvShow.duration
-        text_genre.text = tvShow.genre
-        if (tvShow.tagline != "") {
-            text_tagline.text = tvShow.tagline
-        } else {
-            text_tagline.visibility = GONE
+
+        text_duration.text = String.format(getString(R.string.runtime_format), tvShow.episodeRunTime[0])
+
+        var genre = ""
+        tvShow.genres.forEach {
+            genre += " ${it.name} "
         }
+        text_genre.text = genre
+
+        text_tagline.text = tvShow.tagline
         text_overview.text = tvShow.overview
-        text_creator.text = tvShow.creator
-        text_casts.text = tvShow.casts
+        text_creator.text = tvShow.createdBy[0].name
+        text_networks.text = tvShow.networks[0].name
         text_status.text = tvShow.status
-        text_language.text = tvShow.language
         text_type.text = tvShow.type
-        text_last_season.text = tvShow.lastSeason
-        text_keywords.text = tvShow.keyword
+        text_language.text = tvShow.spokenLanguages[0].name
+        text_production_countries.text = tvShow.productionCountries[0].name
+        text_last_aired_episode.text = String.format(
+            getString(R.string.last_aired_format),
+            tvShow.lastEpisodeToAir.seasonNumber,
+            tvShow.lastEpisodeToAir.episodeNumber,
+            tvShow.lastEpisodeToAir.overview,
+            tvShow.lastEpisodeToAir.airDate
+        )
     }
+
 }
--- app/src/main/java/com/romnan/moviecatalog/ui/detail/tvshows/TvShowDetailViewModel.kt
+++ app/src/main/java/com/romnan/moviecatalog/ui/detail/tvshows/TvShowDetailViewModel.kt
@@ -1,27 +1,45 @@
 package com.romnan.moviecatalog.ui.detail.tvshows
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.romnan.moviecatalog.model.TvShow
-import com.romnan.moviecatalog.util.DataDummy
+import com.romnan.moviecatalog.api.ApiConfig
+import com.romnan.moviecatalog.api.ApiService
+import com.romnan.moviecatalog.model.TvShowDetail
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TvShowDetailViewModel : ViewModel() {
+    private val _tvShowDetail = MutableLiveData<TvShowDetail>()
+    val tvShowDetail: LiveData<TvShowDetail> = _tvShowDetail
 
-    private lateinit var tvShowId: String
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
 
-    fun setSelectedTvShow(tvShowId: String) {
-        this.tvShowId = tvShowId
+    companion object {
+        private const val TAG = "TvShowDetailViewModel"
     }
 
-    fun getTvShow(): TvShow {
-        lateinit var tvShow: TvShow
-        val tvShows = DataDummy.generateDummyTvShows()
-
-        // Search corresponding TvShow
-        for (tvShowItem in tvShows) {
-            if (tvShowItem.id == tvShowId) {
-                tvShow = tvShowItem
+    fun getTvShowDetail(tvShowId: String) {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService()
+            .getTvShowDetail(tvShowId, ApiService.API_KEY, ApiService.VIDEOS)
+        client.enqueue(object : Callback<TvShowDetail> {
+            override fun onResponse(call: Call<TvShowDetail>, response: Response<TvShowDetail>) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _tvShowDetail.value = response.body()
+                } else {
+                    Log.e(TAG, "onResponse: ${response.message()}")
+                }
             }
-        }
-        return tvShow
+
+            override fun onFailure(call: Call<TvShowDetail>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message.toString()}")
+            }
+        })
     }
 }
