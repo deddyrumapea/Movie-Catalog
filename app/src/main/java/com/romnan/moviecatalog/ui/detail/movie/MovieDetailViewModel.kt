2afa92a97c13817eeee41369c2119e53ae7f54b8
Deddy Romnan <romnandeddy@gmail.com>
2020-11-27 21:55:10 +0700

    adjust movie detail data to layout

--- app/src/main/java/com/romnan/moviecatalog/ui/detail/movie/MovieDetailViewModel.kt
+++ app/src/main/java/com/romnan/moviecatalog/ui/detail/movie/MovieDetailViewModel.kt
@@ -1,26 +1,45 @@
 package com.romnan.moviecatalog.ui.detail.movie
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.romnan.moviecatalog.model.Movie
-import com.romnan.moviecatalog.util.DataDummy
+import com.romnan.moviecatalog.api.ApiConfig
+import com.romnan.moviecatalog.api.ApiService
+import com.romnan.moviecatalog.model.MovieDetail
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MovieDetailViewModel : ViewModel() {
-    private lateinit var movieId: String
+    private val _movieDetail = MutableLiveData<MovieDetail>()
+    val movieDetail: LiveData<MovieDetail> = _movieDetail
 
-    fun setSelectedMovie(movieId: String) {
-        this.movieId = movieId
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    companion object {
+        private const val TAG = "MovieDetailViewModel"
     }
 
-    fun getMovie(): Movie {
-        lateinit var movie: Movie
-        val movies = DataDummy.generateDummyMovies()
-
-        // Search for the corresponding movie
-        for (movieItem in movies) {
-            if (movieItem.id == movieId) {
-                movie = movieItem
+    fun getMovieDetail(movieId: String) {
+        _isLoading.value = true
+        val client =
+            ApiConfig.getApiService().getMovieDetail(movieId, ApiService.API_KEY, ApiService.VIDEOS)
+        client.enqueue(object : Callback<MovieDetail> {
+            override fun onResponse(call: Call<MovieDetail>, response: Response<MovieDetail>) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _movieDetail.value = response.body()
+                } else {
+                    Log.e(TAG, "onResponse: ${response.message()}")
+                }
             }
-        }
-        return movie
+
+            override fun onFailure(call: Call<MovieDetail>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message.toString()}")
+            }
+        })
     }
 }
