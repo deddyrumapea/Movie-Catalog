102ca6d0d557c0cfde75868813b4dfe3b0f7082c
Deddy Romnan <romnandeddy@gmail.com>
2020-12-07 10:53:27 +0700

    Update DiscoverActivity.kt

--- app/src/main/java/com/romnan/moviecatalog/ui/discover/DiscoverActivity.kt
+++ app/src/main/java/com/romnan/moviecatalog/ui/discover/DiscoverActivity.kt
@@ -8,13 +8,13 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.romnan.moviecatalog.R
-import kotlinx.android.synthetic.main.activity_catalog.*
+import kotlinx.android.synthetic.main.activity_discover.*
 
 class DiscoverActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_catalog)
+        setContentView(R.layout.activity_discover)
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         view_pager.adapter = sectionsPagerAdapter
