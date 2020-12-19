aabe15e61b0e6a3ca138f3c6edd9e063aa69b63d
Deddy Romnan <romnandeddy@gmail.com>
2020-12-20 00:26:28 +0700

    Create MovieCatalogDatabase.kt

---
+++ app/src/main/java/com/romnan/moviecatalog/data/source/local/room/MovieCatalogDatabase.kt
@@ -0,0 +1,31 @@
+package com.romnan.moviecatalog.data.source.local.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.romnan.moviecatalog.data.model.TvSeriesDetail
+import com.romnan.moviecatalog.data.model.movie.MovieDetail
+
+@Database(
+    entities = [MovieDetail::class, TvSeriesDetail::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class MovieCatalogDatabase : RoomDatabase() {
+    abstract fun favoriteDao(): FavoriteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MovieCatalogDatabase? = null
+
+        fun getInstance(context: Context): MovieCatalogDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    MovieCatalogDatabase::class.java,
+                    "MovieCatalog.db"
+                ).build()
+            }
+    }
+}
