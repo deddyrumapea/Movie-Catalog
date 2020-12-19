4b93c2d88e0bed11513f7883512e839d68dc343a
Deddy Romnan <romnandeddy@gmail.com>
2020-12-20 00:19:11 +0700

    Changed Tv Series Detail Data

--- app/src/main/java/com/romnan/moviecatalog/data/model/tvseries/TvSeriesDetail.kt
+++ app/src/main/java/com/romnan/moviecatalog/data/model/tvseries/TvSeriesDetail.kt
@@ -1,11 +1,14 @@
 package com.romnan.moviecatalog.data.model
 
+import android.os.Parcelable
 import androidx.annotation.NonNull
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
-@Entity(tableName = "tv_series_detail")
+@Parcelize
+@Entity(tableName = "favorite_tv_series")
 data class TvSeriesDetail(
 	@PrimaryKey
 	@NonNull
@@ -24,68 +27,21 @@ data class TvSeriesDetail(
 	@field:SerializedName("vote_average")
 	val voteAverage: Double,
 
-	@field:SerializedName("videos")
-	val videos: Videos,
-
-	@field:SerializedName("episode_run_time")
-	val episodeRunTime: List<Int>,
-
-	@field:SerializedName("genres")
-	val genres: List<GenresItem>,
-
 	@field:SerializedName("tagline")
 	val tagline: String,
 
+	@field:SerializedName("first_air_date")
+	val firstAirDate: String,
+
+	@field:SerializedName("number_of_seasons")
+	val numberOfSeasons: Int,
+
 	@field:SerializedName("overview")
 	val overview: String,
-
-	@field:SerializedName("created_by")
-	val createdBy: List<CreatedByItem>,
-
-	@field:SerializedName("networks")
-	val networks: List<NetworksItem>,
 
 	@field:SerializedName("status")
 	val status: String,
 
-	@field:SerializedName("spoken_languages")
-	val spokenLanguages: List<SpokenLanguagesItem>,
-
 	@field:SerializedName("type")
 	val type: String,
-
-	@field:SerializedName("last_episode_to_air")
-	val lastEpisodeToAir: LastEpisodeToAir,
-
-	@field:SerializedName("production_countries")
-	val productionCountries: List<ProductionCountriesItem>
-)
-
-data class NetworksItem(
-	@field:SerializedName("name")
-	val name: String
-)
-
-data class CreatedByItem(
-	@field:SerializedName("name")
-	val name: String
-)
-
-data class LastEpisodeToAir(
-	@field:SerializedName("air_date")
-	val airDate: String,
-
-	@field:SerializedName("overview")
-	val overview: String,
-
-	@field:SerializedName("episode_number")
-	val episodeNumber: Int,
-
-	@field:SerializedName("season_number")
-	val seasonNumber: Int
-)
-
-data class ProductionCountriesItem(
-	@field:SerializedName("name")
-	val name: String
-)
+) : Parcelable
