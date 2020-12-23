104adcecc773a25ddf1eb870a49dfff46dcfdc19
Deddy Romnan <romnandeddy@gmail.com>
2020-12-23 13:25:57 +0700

    Update FakeMovieCatalogRepository.kt
    updated to latest movie catalog repository

--- app/src/test/java/com/romnan/moviecatalog/data/source/FakeMovieCatalogRepository.kt
+++ app/src/test/java/com/romnan/moviecatalog/data/source/FakeMovieCatalogRepository.kt
@@ -2,38 +2,46 @@ package com.romnan.moviecatalog.data.source
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
 import com.romnan.moviecatalog.data.model.TvSeriesDetail
 import com.romnan.moviecatalog.data.model.movie.MovieDetail
 import com.romnan.moviecatalog.data.model.movie.PopularMovie
 import com.romnan.moviecatalog.data.model.tvseries.PopularTvSeries
+import com.romnan.moviecatalog.data.source.local.LocalDataSource
 import com.romnan.moviecatalog.data.source.remote.RemoteDataSource
+import com.romnan.moviecatalog.utils.AppExecutors
 
-class FakeMovieCatalogRepository(private val remoteDataSource: RemoteDataSource) :
-    MovieCatalogDataSource {
 
-    override fun getPopularMovies(): LiveData<List<PopularMovie>> {
-        val result = MutableLiveData<List<PopularMovie>>()
+class FakeMovieCatalogRepository constructor(
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource,
+    private val appExecutors: AppExecutors
+) : MovieCatalogDataSource {
 
-        remoteDataSource.getPopularMovies(object : RemoteDataSource.LoadPopularMoviesCallback {
-            override fun onPopularMoviesReceived(moviesResponse: List<PopularMovie>) {
-                result.postValue(moviesResponse)
+    companion object {
+        @Volatile
+        private var instance: FakeMovieCatalogRepository? = null
+
+        fun getInstance(
+            remoteDataSource: RemoteDataSource,
+            localDataSource: LocalDataSource,
+            appExecutors: AppExecutors
+        ): FakeMovieCatalogRepository =
+            instance ?: synchronized(this) {
+                instance ?: FakeMovieCatalogRepository(
+                    remoteDataSource,
+                    localDataSource,
+                    appExecutors
+                )
             }
-        })
-
-        return result
     }
 
-    override fun getPopularTvSeries(): LiveData<List<PopularTvSeries>> {
-        val result = MutableLiveData<List<PopularTvSeries>>()
+    override fun getPopularMovies(): DataSource.Factory<Int, PopularMovie> =
+        remoteDataSource.getPopularMovies()
 
-        remoteDataSource.getPopularTvSeries(object : RemoteDataSource.LoadPopularTvSeriesCallback {
-            override fun onPopularTvSeriesReceived(tvSeriesResponse: List<PopularTvSeries>) {
-                result.postValue(tvSeriesResponse)
-            }
-        })
+    override fun getPopularTvSeries(): DataSource.Factory<Int, PopularTvSeries> =
+        remoteDataSource.getPopularTvSeries()
 
-        return result
-    }
 
     override fun getMovieDetail(movieId: Int): LiveData<MovieDetail> {
         val result = MutableLiveData<MovieDetail>()
@@ -60,4 +68,39 @@ class FakeMovieCatalogRepository(private val remoteDataSource: RemoteDataSource)
 
         return result
     }
+
+    override fun insertFavoriteMovie(movie: MovieDetail) =
+        appExecutors.diskIO().execute {
+            localDataSource.insertFavoriteMovie(movie)
+        }
+
+    override fun insertFavoriteTvSeries(tvSeries: TvSeriesDetail) =
+        appExecutors.diskIO().execute {
+            localDataSource.insertFavoriteTvSeries(tvSeries)
+        }
+
+    override fun deleteFavoriteMovie(movie: MovieDetail) =
+        appExecutors.diskIO().execute {
+            localDataSource.deleteFavoriteMovie(movie)
+        }
+
+    override fun deleteFavoriteTvSeries(tvSeries: TvSeriesDetail) =
+        appExecutors.diskIO().execute {
+            localDataSource.deleteFavoriteTvSeries(tvSeries)
+        }
+
+    override fun getFavoriteMovies(): DataSource.Factory<Int, MovieDetail> {
+        return localDataSource.getFavoriteMovies()
+    }
+
+    override fun getFavoriteTvSeries(): DataSource.Factory<Int, TvSeriesDetail> {
+        return localDataSource.getFavoriteTvSeries()
+    }
+
+    override fun isFavoriteMovie(movieId: Int): LiveData<Boolean> =
+        localDataSource.isFavoriteMovie(movieId)
+
+
+    override fun isFavoriteTvSeries(tvSeriesId: Int): LiveData<Boolean> =
+        localDataSource.isFavoriteTvSeries(tvSeriesId)
 }
