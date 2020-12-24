75770075e83d9dc79a8fad10f3541386c63d6183
Deddy Romnan <romnandeddy@gmail.com>
2020-12-24 23:56:41 +0700

    Implement ScrollToBottomAction

--- app/src/androidTest/java/com/romnan/moviecatalog/ui/discover/DiscoverActivityTest.kt
+++ app/src/androidTest/java/com/romnan/moviecatalog/ui/discover/DiscoverActivityTest.kt
@@ -1,26 +1,21 @@
 package com.romnan.moviecatalog.ui.discover
 
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
 import com.romnan.moviecatalog.R
-import com.romnan.moviecatalog.utils.DummyGenerator
+import com.romnan.moviecatalog.ui.util.ScrollToBottomAction
 import com.romnan.moviecatalog.utils.EspressoIdlingResource
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 class DiscoverActivityTest {
-
-    private val dummyPopularMovies = DummyGenerator.getPopularMovies()
-    private val dummyPopularTvSeries = DummyGenerator.getPopularTvSeries()
 
     @Before
     fun setUp() {
@@ -36,22 +31,14 @@ class DiscoverActivityTest {
     @Test
     fun loadPopularMovies() {
         onView(withId(R.id.rv_popular_movies)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_popular_movies)).perform(
-            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                dummyPopularMovies.size
-            )
-        )
+        onView(withId(R.id.rv_popular_movies)).perform(ScrollToBottomAction())
     }
 
     @Test
     fun loadPopularTvSeries() {
         onView(withText(R.string.tv_series)).perform(click())
         onView(withId(R.id.rv_popular_tv_series)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_popular_tv_series)).perform(
-            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                dummyPopularTvSeries.size
-            )
-        )
+        onView(withId(R.id.rv_popular_tv_series)).perform(ScrollToBottomAction())
     }
 
     @Test
